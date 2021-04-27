19f4384d0b44851f99f469a6ca558ec7042f6ad0
kdumagalhaes <ce.magalhaespereira@gmail.com>
2021-04-27 19:15:55 -0300

    finished input validation

--- src/components/atoms/ShortenerInput.vue
+++ src/components/atoms/ShortenerInput.vue
@@ -1,7 +1,20 @@
 <template>
     <form class="shortener-input" @submit.prevent>
-        <input type="text" class="shortener-input__bar" v-model="inputUrl" />
-        <button class="shortener-input__button" @click="putUrl">Shorten</button>
+        <div class="shortener-input__control">
+            <input
+                type="text"
+                class="shortener-input__control--input"
+                v-model="inputUrl"
+                placeholder="http://google.com"
+                autofocus
+            />
+            <button class="shortener-input__control--button" @click="putUrl">
+                Shorten
+            </button>
+        </div>
+        <p v-if="errorMessage !== null" class="shortener-input__error-message">
+            {{ errorMessage }}
+        </p>
     </form>
 </template>
 
@@ -10,11 +23,19 @@ export default {
     data() {
         return {
             inputUrl: '',
+            errorMessage: null,
         }
     },
     methods: {
         putUrl() {
-            this.$store.dispatch('fetchBitLyApi', this.inputUrl)
+            const regex = /^(ftp|http|https):\/\/[^ "]+$/
+
+            if (regex.test(this.inputUrl)) {
+                this.$store.dispatch('fetchBitLyApi', this.inputUrl)
+                this.errorMessage = null
+            } else {
+                this.errorMessage = 'This is not a URL! =/'
+            }
         },
     },
 }
@@ -22,26 +43,35 @@ export default {
 
 <style lang="scss" scoped>
 .shortener-input {
-    display: flex;
     width: 100%;
     margin-bottom: 40px;
 
-    .shortener-input__bar,
-    .shortener-input__button {
+    .shortener-input__error-message {
+        color: $color7;
+        font-weight: $bold;
+        margin: 10px;
+    }
+
+    .shortener-input__control {
+        display: flex;
+    }
+
+    .shortener-input__control--input,
+    .shortener-input__control--button {
         box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
             0 2px 4px -1px rgba(0, 0, 0, 0.06);
         border-radius: 10px;
         height: 60px;
     }
 
-    .shortener-input__bar {
+    .shortener-input__control--input {
         width: 100%;
-        border: 1px solid $color2;
         padding-left: 20px;
         margin-right: 30px;
+        border: 1px solid $color2;
     }
 
-    .shortener-input__button {
+    .shortener-input__control--button {
         width: 100%;
         max-width: 120px;
         border: none;
@@ -54,6 +84,7 @@ export default {
 
         &:hover {
             box-shadow: none;
+            background-color: $color2;
         }
     }
 }
